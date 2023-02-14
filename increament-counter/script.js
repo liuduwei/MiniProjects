c5a0ec21f0a3a0056e19307f3e4804773f142c23
liuduwei <17341730328@163.com>
2023-02-14 12:00:16 +0800

    animation counter

---
+++ increament-counter/script.js
@@ -0,0 +1,44 @@
+const counters = document.querySelectorAll(".counter");
+
+document.addEventListener("DOMContentLoaded", () => {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+    let target = +counter.dataset.target;
+    let current = +counter.innerText;
+    const increament = target / 200;
+    const updateCounter = () => {
+      if (current > target) {
+        counter.innerText = `${target}`;
+      } else {
+        current += Math.ceil(increament);
+        counter.innerText = current;
+        setTimeout(updateCounter, 1);
+      }
+    };
+    updateCounter();
+  });
+});
+
+// const count = function(Counter, timer = 0) {
+//   const intval = setInterval(() => {
+//     timer++;
+//   })
+// }
+
+// const interval1 = setInterval(() => {
+//   console.log(1);
+//   count(5000, timer1, interval1);
+// }, 0.1);
+// const interval2 = setInterval(() => {
+//   console.log(2);
+//   count(7000, timer1, interval2);
+// }, 50);
+// const interval3 = setInterval(() => {
+//   console.log(3);
+//   count(9000, timer1, interval3);
+// }, 50);
+
+// const count = function(counter, timer, interval) {
+//   timer++;
+//   if (timer >= counter) clearInterval(interval);
+// }
