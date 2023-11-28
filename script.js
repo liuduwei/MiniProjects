6c931c92d8f11db498aca8f4a3477a1691c776c8
liuduwei <17341730328@163.com>
2023-11-28 17:59:56 +0800

    refactor blurry-loading

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// blurry loading
+(function () {
+  const heading = document.querySelector(".heading--loading");
+  const bg = document.querySelector(".bg");
+
+  let loading = 0;
+
+  let intval = setInterval(increaseLoading, 30);
+
+  function increaseLoading() {
+    loading++;
+
+    if (loading > 99) {
+      clearInterval(intval);
+    }
+
+    renderBlur(loading);
+  }
+  const renderBlur = function (loading) {
+    bg.style.filter = `blur(${scale(loading, 0, 100, 30, 0)}px)`;
+    heading.style.opacity = scale(loading, 0, 100, 1, 0);
+    heading.innerText = `${loading}%`;
+  };
+
+  const scale = function (num, in_min, in_max, out_min, out_max) {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+})();
+
 // animation-nav
 const animation_nav_btnEl = document.querySelector(".animation-nav .btn");
 const animation_nav_navEl = document.querySelector(".animation-nav .nav");
