bea44261ca6452f8fdcace405fee7632b6874b73
liuduwei <17341730328@163.com>
2023-11-28 19:34:29 +0800

    refactor content_placeholder

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-// blurry loading
+// click_button
+(function () {
+  const btn = document.querySelector(".button--click");
+
+  btn.addEventListener("click", (e) => {
+    const clickEl = document.createElement("div");
+    const xAxis = e.offsetX;
+    const yAxis = e.offsetY;
+    console.log(xAxis);
+    console.log(yAxis);
+    clickEl.style.cssText = `
+    
+    left: ${xAxis}px;
+    top: ${yAxis}px;
+    `;
+    clickEl.classList.add("cirlce");
+    e.target.appendChild(clickEl);
+
+    setTimeout(() => {
+      clickEl.remove();
+    }, 500);
+  });
+})();
+let newLinePreventPrettier = "";
 (function () {
   const heading = document.querySelector(".heading--loading");
   const bg = document.querySelector(".bg");
